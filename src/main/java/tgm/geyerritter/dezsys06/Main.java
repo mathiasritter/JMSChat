0e60bef59c0b1a5f6e365523a573adb4c498a354
Mathias Ritter <mritter@student.tgm.ac.at>
2014-11-20 19:36:11 +0100

    Main & Kommentare hinzugefuegt

---
+++ src/main/java/tgm/geyerritter/dezsys06/Main.java
@@ -0,0 +1,23 @@
+package tgm.geyerritter.dezsys06;
+
+import tgm.geyerritter.dezsys06.io.ChatConsoleReader;
+
+/**
+ * Main-Klasse mit Main-Methode zum Start des Chats
+ * 
+ * @author mritter
+ * @version sgeyer
+ */
+public class Main {
+
+	/**
+	 * Main-Methode zum Start des Chats
+	 * 
+	 * @param args Kommandozeilenargumente
+	 */
+	public static void main(String[] args) {
+		//Chat in einem neuen Thread starten
+		new Thread(new ChatConsoleReader()).start();
+	}
+	
+}
