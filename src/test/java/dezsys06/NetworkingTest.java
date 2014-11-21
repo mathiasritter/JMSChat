447f3f4ad69cdd654771560c79e7b719e2434bd2
Stefan <sgeyer@student.tgm.ac.at>
2014-11-21 19:50:12 +0100

    Testcases erweitert

--- src/test/java/dezsys06/NetworkingTest.java
+++ src/test/java/dezsys06/NetworkingTest.java
@@ -1,47 +1,57 @@
 package dezsys06;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.After;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
+import tgm.geyerritter.dezsys06.data.ExplicitConfiguration;
 import tgm.geyerritter.dezsys06.data.StaticConfiguration;
 import tgm.geyerritter.dezsys06.net.NetworkController;
 import tgm.geyerritter.dezsys06.net.Networking;
 
 public class NetworkingTest {
 
-	private ByteArrayOutputStream outContent;
+	private TestAppender testAppender;
+	private final int timeout = 100;
 	
 	@Before
-	public void setUpStreams() {
-		outContent = new ByteArrayOutputStream();
-	    System.setOut(new PrintStream(outContent));
+	public void setup() {
+		this.testAppender = new TestAppender();
+		Logger.getRootLogger().addAppender(testAppender);
 	}
-	
+
 	@Test
-	public void testNetworking() {
+	public void testNetworking() throws InterruptedException {
 		NetworkController n = new Networking("test", new StaticConfiguration());
 		n.broadcast("Ich bin eine Nachricht");
-		assertEquals("Ich bin eine Nachricht", "");
+		Thread.sleep(timeout);
+		assertTrue(((String) this.testAppender.getLog().get(0).getMessage()).contains("Ich bin eine Nachricht"));
 	}
-	
+
 	@Test(expected = NullPointerException.class)
 	public void testNetworking1() {
 		NetworkController n = new Networking(null, new StaticConfiguration());
 	}
-	
+
 	@Test(expected = NullPointerException.class)
 	public void testNetworking2() {
 		NetworkController n = new Networking("", null);
 	}
 	
-	@After
-	public void cleanUpStreams() {
-	    System.setOut(null);
+	@Test
+	public void testNetworking3() throws InterruptedException {
+		NetworkController n = new Networking("Dondi", new StaticConfiguration());
+		n.mail("Dondi", "Fibern's doch mit!");
+		n.getMails();
+		assertEquals(" Dondi: Fibern's doch mit!", ((String)this.testAppender.getLog().get(1).getMessage()).substring(((String)this.testAppender.getLog().get(1).getMessage()).indexOf(']') + 1, ((String)this.testAppender.getLog().get(1).getMessage()).length()));
+	}
+	
+	@Test
+	public void testNetworking4() {
+		NetworkController n = new Networking("Dondi", new ExplicitConfiguration(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, ActiveMQConnection.DEFAULT_BROKER_URL, "Chat"));
+		n.halt();
 	}
 }
