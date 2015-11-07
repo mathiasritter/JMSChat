f9d5ac62ebd64f01fcb8359a48b6d92f103faee1
serceg-tgm <serceg@student.tgm.ac.at>
2015-11-07 10:56:47 +0100

    added chat-window

--- src/main/java/tgm/geyerritter/dezsys06/gui/ChatWindow.java
+++ src/main/java/tgm/geyerritter/dezsys06/gui/ChatWindow.java
@@ -1,10 +1,23 @@
 package tgm.geyerritter.dezsys06.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.apache.log4j.Logger;
+import tgm.geyerritter.dezsys06.io.ChatConsoleReader;
 
+import java.awt.*;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 /**
@@ -12,22 +25,72 @@ import java.util.ResourceBundle;
  */
 public class ChatWindow extends Application implements Initializable, GUIPrinter {
 
-    public ChatWindow() {
+    private Pane root;
+
+    @FXML
+    private TextArea showMessages;
+
+    @FXML
+    private TextField writeMessage;
+
+    @FXML
+    private Button sendMessage;
+
+    private ChatConsoleReader chatConsoleReader;
+
+    public ChatWindow() throws IOException {
+
+        Logger.getRootLogger().addAppender(new GUIAppender(this));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("gui/chatwindow.fxml"));
+        loader.setController(this);
+
+        this.chatConsoleReader = ChatConsoleReader.getInstance();
+
+        try {
+            this.root = (GridPane) loader.load();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
     }
     
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        Scene scene = new Scene(this.root);
+
+        primaryStage.setTitle("Chat-Window");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        sendMessage.setOnAction((event) -> {
+
+            String line = writeMessage.getText();
+            String label = "";
+            String[] args = line.split(" ");
+            if (args.length > 0) {
+                label = args[0];
+                args = Arrays.copyOfRange(args, 1, args.length);
+            }
+
+            this.chatConsoleReader.proccessCommand(label, args);
+
+
+        });
+
     }
 
     @Override
     public void print(String text) {
+
+        showMessages.append(text);
 
     }
 }
