475823d2e12e2c2cc493714de8d0fb0d7ef77ebf
Mathias Ritter <mritter@student.tgm.ac.at>
2015-11-07 09:16:59 +0100

    added classes for java fx gui

---
+++ src/main/java/tgm/geyerritter/dezsys06/gui/ConnectWindow.java
@@ -0,0 +1,28 @@
+package tgm.geyerritter.dezsys06.gui;
+
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * TODO class comment
+ */
+public class ConnectWindow extends Application implements Initializable {
+
+    public ConnectWindow() {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
